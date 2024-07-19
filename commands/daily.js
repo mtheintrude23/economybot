f0cbbe2abcb9835f89beaf6b37cc20d23fb93499
mtheintrude23 <167693071+mtheintrude23@users.noreply.github.com>
2024-07-19 10:58:32 +0700

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -3,7 +3,7 @@ const db = require("quick.db");
 const ms = require("parse-ms");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.content.startsWith('m!'))return;  
+  if(!message.content.startsWith('m/'))return;  
 
   let user = message.author;
 
@@ -17,22 +17,20 @@ module.exports.run = async (bot, message, args) => {
   
     let timeEmbed = new Discord.RichEmbed()
     .setColor("#FFFFFF")
-    .setDescription(`<:Cross:618736602901905418> You've already collected your daily reward\n\nCollect it again in ${time.hours}h ${time.minutes}m ${time.seconds}s `);
+    .setDescription(`<:failed:1231261075886833876> Bạn đã nhận phần thưởng hàng ngày\n\nBạn có thể nhận lại sau ${time.hours} giờ ${time.minutes} phút ${time.seconds} giây`);
     message.channel.send(timeEmbed)
   } else {
     let moneyEmbed = new Discord.RichEmbed()
-  .setColor("#FFFFFF")
-  .setDescription(`<:Check:618736570337591296> You've collected your daily reward of ${amount} coins`);
-  message.channel.send(moneyEmbed)
-  db.add(`money_${message.guild.id}_${user.id}`, amount)
-  db.set(`daily_${message.guild.id}_${user.id}`, Date.now())
-
-
+    .setColor("#FFFFFF")
+    .setDescription(`<a:Tick:1228712635856453712> Bạn đã nhận được phần thưởng hàng ngày là ${amount} Mcash`);
+    message.channel.send(moneyEmbed)
+    db.add(`money_${message.guild.id}_${user.id}`, amount)
+    db.set(`daily_${message.guild.id}_${user.id}`, Date.now())
   }
 };
 
-
 module.exports.help = {
-  name:"daily",
-  aliases: ["day"]
-}
+  name: "daily",
+  aliases: ["day"],
+  description: "Lệnh này cho phép bạn nhận phần thưởng hàng ngày"
+};
