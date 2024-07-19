3f075554c628e91542cb747dc3f0e51b39785a18
mtheintrude23 <167693071+mtheintrude23@users.noreply.github.com>
2024-07-19 10:51:06 +0700

    Update beg.js

--- commands/beg.js
+++ commands/beg.js
@@ -3,12 +3,12 @@ const db = require("quick.db");
 const ms = require("parse-ms");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.content.startsWith('m!'))return;  
+  if(!message.content.startsWith('m/'))return;  
 
   let user = message.author;
 
   let timeout = 180000;
-  let amount = 5;
+  let amount = 1000;
 
   let beg = await db.fetch(`beg_${message.guild.id}_${user.id}`);
 
@@ -17,12 +17,12 @@ module.exports.run = async (bot, message, args) => {
   
     let timeEmbed = new Discord.RichEmbed()
     .setColor("#FFFFFF")
-    .setDescription(`<:Cross:618736602901905418> You've already begged recently\n\nBeg again in ${time.minutes}m ${time.seconds}s `);
+    .setDescription(`<:failed:1231261075886833876> Gần đây bạn đã ăn xin\n\nHãy ăn xin lại ${time.minutes}m ${time.seconds}s `);
     message.channel.send(timeEmbed)
   } else {
     let moneyEmbed = new Discord.RichEmbed()
   .setColor("#FFFFFF")
-  .setDescription(`<:Check:618736570337591296> You've begged and received ${amount} coins`);
+  .setDescription(`<a:Tick:1228712635856453712> Bạn đã ăn xin và nhận được ${amount} Mcash`);
   message.channel.send(moneyEmbed)
   db.add(`money_${message.guild.id}_${user.id}`, amount)
   db.set(`beg_${message.guild.id}_${user.id}`, Date.now())
