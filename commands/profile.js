d439f396305e6760992e5ce7233e0aba33a093bb
mtheintrude23 <167693071+mtheintrude23@users.noreply.github.com>
2024-07-19 11:04:04 +0700

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -2,36 +2,37 @@ const Discord = require("discord.js");
 const db = require("quick.db");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.content.startsWith('m!'))return;  
+  if (!message.content.startsWith('m/')) return;  
 
   let user = message.mentions.members.first() || message.author;
 
-  let money = await db.fetch(`money_${message.guild.id}_${user.id}`)
+  let money = await db.fetch(`money_${message.guild.id}_${user.id}`);
   if (money === null) money = 0;
 
-  let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`)
+  let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`);
   if (bank === null) bank = 0;
 
-  let vip = await db.fetch(`bronze_${message.guild.id}_${user.id}`)
-    if(vip === null) vip = 'None'
-    if(vip === true) vip = 'Bronze'
+  let vip = await db.fetch(`bronze_${message.guild.id}_${user.id}`);
+  if (vip === null) vip = 'None';
+  if (vip === true) vip = 'Bronze';
 
-  let shoes = await db.fetch(`nikes_${message.guild.id}_${user.id}`)
-  if(shoes === null) shoes = '0'
+  let shoes = await db.fetch(`nikes_${message.guild.id}_${user.id}`);
+  if (shoes === null) shoes = '0';
 
-  let newcar = await db.fetch(`car_${message.guild.id}_${user.id}`)
-  if(newcar === null) newcar = '0'
+  let newcar = await db.fetch(`car_${message.guild.id}_${user.id}`);
+  if (newcar === null) newcar = '0';
 
-  let newhouse = await db.fetch(`house_${message.guild.id}_${user.id}`)
-  if(newhouse === null) newhouse = '0'
+  let newhouse = await db.fetch(`house_${message.guild.id}_${user.id}`);
+  if (newhouse === null) newhouse = '0';
 
   let moneyEmbed = new Discord.RichEmbed()
-  .setColor("#FFFFFF")
-  .setDescription(`**${user}'s Profile**\n\nPocket: ${money}\nBank: ${bank}\nVIP Rank: ${vip}\n\n**Inventory**\n\nNikes: ${shoes}\nCars: ${newcar}\nMansions: ${newhouse}`);
-  message.channel.send(moneyEmbed)
+    .setColor("#FFFFFF")
+    .setDescription(`**Hồ Sơ Của ${user}**\n\nTúi: ${money} xu\nNgân Hàng: ${bank} xu\nCấp VIP: ${vip}\n\n**Kho**\n\nNikes: ${shoes}\nXe Hơi: ${newcar}\nBiệt Thự: ${newhouse}`);
+  message.channel.send(moneyEmbed);
 };
 
 module.exports.help = {
-  name:"profile",
-  aliases: ["pro"]
-}
+  name: "profile",
+  aliases: ["pro"],
+  description: "Lệnh này hiển thị hồ sơ và thông tin của người dùng"
+};
