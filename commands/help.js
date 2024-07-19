f1b1712641eed00e0b593c6ad59f5d3d719b3c02
mtheintrude23 <167693071+mtheintrude23@users.noreply.github.com>
2024-07-19 11:01:00 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,24 +1,20 @@
-const Discord = require('discord.js')
-const db = require('quick.db')
+const Discord = require('discord.js');
+const db = require('quick.db');
 
 module.exports.run = async (bot, message, args) => {
-    if(!message.content.startsWith('m!'))return;  
-
+    if(!message.content.startsWith('m/'))return;  
 
     let embed = new Discord.RichEmbed()
-    .setTitle("Money Man Help Centre [Prefix m!]")
-    .addField("Economy Commands", "`work` `beg` `rob` `pay` `balance` `profile` `withdraw` `deposit` `daily` `weekly` `store` `buy` `sell`")
-    .addField("Gambling Commmands", "`roulette` `slots`")
-    .addField("Economy Extra Commands", "`storeinfo [item]`")
-    .setColor("#FFFFFF")
-    message.channel.send(embed)
-
-
-
-
+    .setTitle("Trung Tâm Trợ Giúp Economy Bot [Prefix m/]")
+    .addField("Lệnh Kinh Tế", "`work` `beg` `rob` `pay` `balance` `profile` `withdraw` `deposit` `daily` `weekly` `store` `buy` `sell`")
+    .addField("Lệnh Cờ Bạc", "`roulette` `slots`")
+    .addField("Lệnh Kinh Tế Bổ Sung", "`storeinfo [item]`")
+    .setColor("#FFFFFF");
+    message.channel.send(embed);
 }
 
 module.exports.help = {
-  name:"help",
-  aliases: [""]
+  name: "help",
+  aliases: [""],
+  description: "Lệnh này hiển thị các lệnh có sẵn trong bot"
 }
