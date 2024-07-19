95d402f6d1b3d9535b6e8cc7911c6f2ff6e8d620
mtheintrude23 <167693071+mtheintrude23@users.noreply.github.com>
2024-07-19 11:00:09 +0700

    Update deposit.js

--- commands/deposit.js
+++ commands/deposit.js
@@ -3,65 +3,67 @@ const db = require("quick.db");
 const ms = require("parse-ms");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.content.startsWith('m!'))return;  
+  if (!message.content.startsWith('m/')) return;
 
   let user = message.author;
 
-  let member = db.fetch(`money_${message.guild.id}_${user.id}`)
-  let member2 = db.fetch(`bank_${message.guild.id}_${user.id}`)
+  let member = db.fetch(`money_${message.guild.id}_${user.id}`);
+  let member2 = db.fetch(`bank_${message.guild.id}_${user.id}`);
 
   if (args[0] == 'all') {
-    let money = await db.fetch(`money_${message.guild.id}_${user.id}`)
-    let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`)
+    let money = await db.fetch(`money_${message.guild.id}_${user.id}`);
+    let bank = await db.fetch(`bank_${message.guild.id}_${user.id}`);
 
     let embedbank = new Discord.RichEmbed()
-    .setColor('#FFFFFF')
-    .setDescription("<:Cross:618736602901905418> You don't have any money to deposit")
+      .setColor('#FFFFFF')
+      .setDescription("<:failed:1231261075886833876> Bạn không có tiền để gửi");
 
-    if(money === 0) return message.channel.send(embedbank)
+    if (money === 0) return message.channel.send(embedbank);
 
-    db.add(`bank_${message.guild.id}_${user.id}`, money)
-    db.subtract(`money_${message.guild.id}_${user.id}`, money)
+    db.add(`bank_${message.guild.id}_${user.id}`, money);
+    db.subtract(`money_${message.guild.id}_${user.id}`, money);
     let embed5 = new Discord.RichEmbed()
-  .setColor("#FFFFFF")
-  .setDescription(`<:Check:618736570337591296> You have deposited all your coins into your bank`);
-  message.channel.send(embed5)
-  
+      .setColor("#FFFFFF")
+      .setDescription(`<a:Tick:1228712635856453712> Bạn đã gửi tất cả tiền của mình vào ngân hàng`);
+    message.channel.send(embed5);
+
   } else {
-  
-  let embed2 = new Discord.RichEmbed()
-  .setColor("#FFFFFF")
-  .setDescription(`<:Cross:618736602901905418> Specify an amount to deposit`);
-  
-  if (!args[0]) {
+
+    let embed2 = new Discord.RichEmbed()
+      .setColor("#FFFFFF")
+      .setDescription(`<:failed:1231261075886833876> Vui lòng chỉ định số tiền để gửi`);
+
+    if (!args[0]) {
       return message.channel.send(embed2)
-      .catch(err => console.log(err))
-  }
-  let embed3 = new Discord.RichEmbed()
-  .setColor("#FFFFFF")
-  .setDescription(`<:Cross:618736602901905418> You can't deposit negative money`);
+        .catch(err => console.log(err));
+    }
+    let embed3 = new Discord.RichEmbed()
+      .setColor("#FFFFFF")
+      .setDescription(`<:failed:1231261075886833876> Bạn không thể gửi số tiền âm`);
 
-  if (message.content.includes('-')) { 
-      return message.channel.send(embed3)
-  }
-  let embed4 = new Discord.RichEmbed()
-  .setColor("#FFFFFF")
-  .setDescription(`<:Cross:618736602901905418> You don't have that much money`);
+    if (message.content.includes('-')) {
+      return message.channel.send(embed3);
+    }
+    let embed4 = new Discord.RichEmbed()
+      .setColor("#FFFFFF")
+      .setDescription(`<:failed:1231261075886833876> Bạn không có đủ tiền để gửi`);
 
-  if (member < args[0]) {
-      return message.channel.send(embed4)
-  }
+    if (member < args[0]) {
+      return message.channel.send(embed4);
+    }
 
-  let embed5 = new Discord.RichEmbed()
-  .setColor("#FFFFFF")
-  .setDescription(`<:Check:618736570337591296> You have deposited ${args[0]} coins into your bank`);
+    let embed5 = new Discord.RichEmbed()
+      .setColor("#FFFFFF")
+      .setDescription(`<a:Tick:1228712635856453712> Bạn đã gửi ${args[0]} Mcash vào ngân hàng`);
 
-  message.channel.send(embed5)
-  db.add(`bank_${message.guild.id}_${user.id}`, args[0])
-  db.subtract(`money_${message.guild.id}_${user.id}`, args[0])
+    message.channel.send(embed5);
+    db.add(`bank_${message.guild.id}_${user.id}`, args[0]);
+    db.subtract(`money_${message.guild.id}_${user.id}`, args[0]);
   }
 }
+
 module.exports.help = {
-  name:"deposit",
-  aliases: ["dep"]
+  name: "deposit",
+  aliases: ["dep"],
+  description: "Lệnh này cho phép bạn gửi tiền vào ngân hàng"
 }
